0e73cd3808c28f2797342ecc89e509f15a3fe728
Tiemi Faustino <hayashipharma@gmail.com>
2021-11-29 19:18:18 -0300

    Exercício 2 feito

---
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/4.2-exercicio2.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1. somar todos os valores contidos no array
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    sum = sum + numbers[i];
+}
+
+console.log(sum);
