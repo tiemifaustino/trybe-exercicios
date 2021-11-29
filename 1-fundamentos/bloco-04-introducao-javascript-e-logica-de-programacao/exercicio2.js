d69a5f9a926a687bae621f2b51c529d2dec8e05a
Tiemi Faustino <hayashipharma@gmail.com>
2021-11-29 19:00:25 -0300

    Exercício 1 feito

--- 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/exercicio2.js
+++
@@ -1,8 +0,0 @@
-const x = 4;
-const y = 2;
-
-if (y > x) {
-    console.log("y é maior que x");
-} else {
-    console.log("x é maior que y");
-}
--- 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/exercicio5.js
+++
@@ -1,4 +0,0 @@
-const angulo1 = 10;
-const angulo2 = 45;
-const angulo3 = 30;
-
