957b8c9a68275291d0df99aeb9831b8e0c270ca2
Tiemi Faustino <hayashipharma@gmail.com>
2021-11-29 18:44:00 -0300

    Exercícios bloco 4 dia 1 realocados ao diretório dia 4.1

---
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercicio4.js
@@ -0,0 +1,9 @@
+const valor = -1;
+
+if (valor > 0) {
+    console.log("positive");
+} else if (valor < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
---
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercicio5.js
@@ -0,0 +1,7 @@
+const angulo1 = 10;
+const angulo2 = 45;
+const angulo3 = 30;
+
+if (angulo1 + angulo2 + angulo3 === 180) {
+    
+}
