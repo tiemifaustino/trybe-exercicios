019fd3a6048d101b46f81f70a18143fd1f93aa39
Tiemi Faustino <hayashipharma@gmail.com>
2021-11-30 19:40:41 -0300

    Exercício 1 feito

--- 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio1.js
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio1.js
@@ -1,9 +1,12 @@
 let n = 4;
 
-let quadrado = "";
 for (let coluna = 0; coluna < n; coluna += 1) {
-    for (let linha = 0; linha < n; linha += 1) {
-        quadrado += "*";
-        console.log(quadrado);
+    let quadrado = "";
+        for (let linha = 0; linha < n; linha += 1) {
+        quadrado += "*";  
     }
+    console.log(quadrado); 
 }
+
+
+// for (let linha = 0; linha < n; linha += 1) {
---
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio3.js
@@ -0,0 +1,7 @@
+let n = 5;
+
+let trianguloInvertido= "";
+for (let coluna = 0; coluna < n; coluna += 1) {
+    trianguloInvertido += "*";
+    console.log(trianguloInvertido);
+}
