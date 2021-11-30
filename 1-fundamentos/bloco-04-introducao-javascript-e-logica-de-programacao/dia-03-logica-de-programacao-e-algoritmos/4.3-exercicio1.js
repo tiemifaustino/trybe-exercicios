4cb57f848886c286507f0fb8ca320f3d0900d223
Tiemi Faustino <hayashipharma@gmail.com>
2021-11-30 20:18:22 -0300

    Exercício 1 feito com comentários da execução

--- 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio1.js
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio1.js
@@ -1,4 +1,4 @@
-let n = 4;
+let n = 5;
 
 for (let coluna = 0; coluna < n; coluna += 1) {
     let quadrado = "";
@@ -8,5 +8,10 @@ for (let coluna = 0; coluna < n; coluna += 1) {
     console.log(quadrado); 
 }
 
-
-// for (let linha = 0; linha < n; linha += 1) {
+/* 
+1. Fazer um "for" para rodar a quantidade de vezes do valor "n"
+2. Declarar uma variável com string vazia para armazenar os asteriscos dentro desse "for"
+3. Fazer outro "for" dentro do 1º "for" para repetir a quantidade de asteriscos até o valor de "n", isso irá fazer uma linha com a quantidade do valor de "n"
+4. Após o "for" que está dentro rodar a quantidade de vezes de "n" ele irá pro "for" de fora e irá executar o comando que está dentro do "for" de fora, imprimindo uma linha
+5. O processo do "for" de fora irá ser executado "n" vezes imprimindo assim várias linhas até o valor de "n"
+*/
