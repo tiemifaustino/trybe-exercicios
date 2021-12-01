4ca87320d0a9ff6c390980b0dc1cc5e93260b062
Tiemi Faustino <hayashipharma@gmail.com>
2021-12-01 18:22:19 -0300

    Exercício 4 - parte 1 feito

---
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicio4.js
@@ -0,0 +1,13 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+info.recorrente = 'Sim';
+
+/* 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console: */
+
+for (let key in info) {
+  console.log(info[key]);
+}
