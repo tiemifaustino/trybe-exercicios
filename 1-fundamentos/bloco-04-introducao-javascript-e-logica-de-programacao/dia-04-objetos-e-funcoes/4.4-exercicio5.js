2530ccf023fabfbf09087cfaf91e5b5932521b34
Tiemi Faustino <hayashipharma@gmail.com>
2021-12-01 19:16:56 -0300

    Exercício 5 - parte 1 feito

---
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicio5.js
@@ -0,0 +1,29 @@
+/* 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console: */
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+info.recorrente = 'Sim';
+
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+
+for (let properties in info) {
+  if (properties === "recorrente" && info[properties] === 'Sim' && info2[properties] === 'Sim') {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[properties] + " e " + info2[properties]);
+  }
+}
+
+
+
