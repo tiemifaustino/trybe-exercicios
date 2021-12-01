c336262b4758e6a53be2cf7ed6ca73f0f17f8660
Tiemi Faustino <hayashipharma@gmail.com>
2021-12-01 10:27:32 -0300

    Exercício 2 adição de comentários

--- 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio2.js
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio2.js
@@ -5,3 +5,11 @@ for (let coluna = 0; coluna < n; coluna += 1) {
     trianguloRetangulo += "*";
     console.log(trianguloRetangulo);
 }
+
+/* 
+1. Declarar uma variável para armazenar os asteriscos
+2. Fazer um "for" para rodar até "n" vezes, no caso 5 vezes (0, 1, 2, 3, 4)
+3. Comando de adicionar um asterisco na variável a cada rodada
+4. Imprimir a variável de asteriscos atualizada a cada rodada
+5. Rodada 1 - *, rodada 2 - ** e assim por diante
+ */
