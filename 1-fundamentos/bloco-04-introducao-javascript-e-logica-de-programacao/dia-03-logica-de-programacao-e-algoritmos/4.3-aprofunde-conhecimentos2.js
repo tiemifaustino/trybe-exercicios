62df0b29f5402bd4f1ac98cec5b7f7cde2216ae3
Tiemi Faustino <hayashipharma@gmail.com>
2021-12-01 11:12:38 -0300

    Exercício 2 aprofunde seus conhecimentos feito

---
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-aprofunde-conhecimentos2.js
@@ -0,0 +1,19 @@
+/* 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente. */
+
+let word = 'tryber';
+
+let reverse = "";
+
+for (let i = word.length - 1; i >= 0; i -= 1) {
+    reverse = reverse + word[i];
+}
+console.log(reverse);
+
+// 1. Declarar uma variável para armazenar a palavra invertida (reverse)
+// 2. Fazer um "for" para rodar de forma decrescente na string word ( -= 1 )
+        // a condição de início é a última posição da string que é o tamanho total - 1
+        // a condição de parada é até ser maior ou igual a zero (primeira posição da string)
+// 3. Adicionar na variável "reverse" cada letra (posição da letra na string) para ir concatenando
+// 4. Imprimir a variável "reverse" com a palavra invertida
+console.log(word.length);
+console.log(word[1]);
