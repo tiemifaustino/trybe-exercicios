84a63ded973af8ce6e20821af808289de4367539
Tiemi Faustino <hayashipharma@gmail.com>
2021-12-01 10:21:03 -0300

    Exercício 1 adição de mais comentários

--- 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio1.js
+++ 1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/4.3-exercicio1.js
@@ -13,5 +13,6 @@ for (let coluna = 0; coluna < n; coluna += 1) {
 2. Declarar uma variável com string vazia para armazenar os asteriscos dentro desse "for"
 3. Fazer outro "for" dentro do 1º "for" para repetir a quantidade de asteriscos até o valor de "n", isso irá fazer uma linha com a quantidade do valor de "n"
 4. Após o "for" que está dentro rodar a quantidade de vezes de "n" ele irá pro "for" de fora e irá executar o comando que está dentro do "for" de fora, imprimindo uma linha
-5. O processo do "for" de fora irá ser executado "n" vezes imprimindo assim várias linhas até o valor de "n"
+5.Após ele imprimir uma linha, ele executa novamente "n" vezes o "for" de dentro e imprime outra linha, e depois repete o ciclo até o numero "n" de vezes 
+6. O processo do "for" de fora irá ser executado "n" vezes imprimindo assim várias linhas até o valor de "n"
 */
