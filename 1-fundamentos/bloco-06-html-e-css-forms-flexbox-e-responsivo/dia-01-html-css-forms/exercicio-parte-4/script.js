79da6cffc1f7e175050527df1293fa209427a5fd
Tiemi Faustino <hayashipharma@gmail.com>
2022-01-04 19:05:36 -0300

    Atividade 02 parte-04 ok

--- 1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/exercicio-parte-4/script.js
+++ 1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/exercicio-parte-4/script.js
@@ -4,7 +4,13 @@ const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
 
 /* 1 - Adicione um evento click no elemento a que possui o id href . Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer. */
-function prevent(event) {
+function preventLink(event) {
     event.preventDefault();
 }
-HREF_LINK.addEventListener('click', prevent);
+HREF_LINK.addEventListener('click', preventLink);
+
+/* 2 - Adicione um evento click no elemento checkbox que possui o id input-checkbox . Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer. */
+function preventInput(event) {
+    event.preventDefault();
+}
+INPUT_CHECKBOX.addEventListener('click', preventInput);
