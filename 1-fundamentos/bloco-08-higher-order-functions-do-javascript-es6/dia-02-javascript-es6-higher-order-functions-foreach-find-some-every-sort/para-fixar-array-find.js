e00e0ab9a09b3eb549376a4531aaecc7b83af35e
Tiemi Faustino <hayashipharma@gmail.com>
2022-01-19 18:55:48 -0300

    Exercício 1 para fixar array.find feito

---
+++ 1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/para-fixar-array-find.js
@@ -0,0 +1,15 @@
+// 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (array) => {
+  // Adiciona seu código aqui
+  const divisibleNumber = array.find((element) => element % 3 === 0 && element % 5 === 0);
+  return divisibleNumber;
+  // ou como no gabarito:
+  // return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+  // LEMBRAR de colocar o return!!!
+}
+
+console.log(findDivisibleBy3And5(numbers));
+
