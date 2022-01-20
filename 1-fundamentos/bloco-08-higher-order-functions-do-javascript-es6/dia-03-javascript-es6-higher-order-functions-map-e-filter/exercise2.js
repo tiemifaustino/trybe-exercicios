12d9028ebb1943771c9ae3325b5b2462087d25fa
Tiemi Faustino <hayashipharma@gmail.com>
2022-01-20 19:10:21 -0300

    Exercício 2 ok

--- 1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise2.js
+++ 1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise2.js
@@ -60,13 +60,17 @@ const books = [
         releaseYear: 1928,
     },
 ];
-  
-// Adicione o código do exercício aqui:
-//   1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
-//   Dica: Use a função map
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// Dica: use as funções map , sort
 
-const arrayStringsBook = (array) => books.map((bookName) => `${bookName.name} - ${bookName.genre} - ${bookName.author.name}`);
+function nameAndAge() {
+    // escreva seu código aqui
+    return books.map((book) => (
+        {
+          age: book.releaseYear - book.author.birthYear,
+          author: book.author.name
+        }
+    )).sort((alpha, beta) => alpha.age - beta.age);
+}
 
-console.log(arrayStringsBook(books));
-
-
+console.log(nameAndAge(books));
