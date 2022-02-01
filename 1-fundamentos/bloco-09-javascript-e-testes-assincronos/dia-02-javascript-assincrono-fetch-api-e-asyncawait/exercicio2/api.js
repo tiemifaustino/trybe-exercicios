0960f9c72472331d6762f9601ef992faa6fa357c
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-01 11:58:46 -0300

    exercicio 2.4 função refatorada para mostrar somente as 10 primeiras moedas usando o .filter()

--- 1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/exercicio2/api.js
+++ 1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/exercicio2/api.js
@@ -16,17 +16,23 @@ async function insertDOM() {
     const coins = await fetchArrayCrypto();
     // array de objetos contendo as moedas
 
+    const topTen = coins.filter((element, index) => index < 10)
+    // filtra somente as 10 primeiras moedas
+    // outra forma = .filter((coin) => Number(coin.rank) <= 10)
+
+    console.log(topTen);
+
     const coinList = document.getElementById('lista-crypto');
     // constante para armazenar a ul, onde serão inseridas as li 
 
-    coins.forEach((coin) => {
+    topTen.forEach((coin) => {
         // com o forEach ele percorre pela lista de moedas
 
         const newLi = document.createElement('li');
         // cria uma li para cada moeda
 
         const usdPrice = Number(coin.priceUsd);
-        // converte em número o valor do preço de cada moeda
+        // converte em número o valor do preço de cada moeda pois o valor está como uma string
 
         newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
         // insere o texto para cada moeda, contendo o nome, símbolo e preço com 2 casas decimais
