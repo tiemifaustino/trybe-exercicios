0d06e427083704995a0a901ee9ee54572ca7b51d
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-01 11:49:43 -0300

    exercicio 2.3 em andamento

--- 1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/exercicio2/api.js
+++ 1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/exercicio2/api.js
@@ -3,8 +3,38 @@ async function fetchArrayCrypto() {
       .then((response) => response.json())
       .then((data) => data.data)
       .catch((error) => error.toString());
-    
-    console.log(coins);
+    // o fetch retorna uma promise que contém o dado final que queremos acessar
+    // se sucesso ele retorna o objeto JSON - .then((response) => response.json())
+    // acesso à chave "data" do objeto JSON onde estão as moedas- .then((data) => data.data)
+    // se falha "pegar" o erro e retorna uma string representando o objeto Error - .catch((error) => error.toString());
+
+    return coins;
+    // retorna um array de objetos, cada objeto é uma moeda
 }
 
-fetchArrayCrypto();
+async function insertDOM() {
+    const coins = await fetchArrayCrypto();
+    // array de objetos contendo as moedas
+
+    const coinList = document.getElementById('lista-crypto');
+    // constante para armazenar a ul, onde serão inseridas as li 
+
+    coins.forEach((coin) => {
+        // com o forEach ele percorre pela lista de moedas
+
+        const newLi = document.createElement('li');
+        // cria uma li para cada moeda
+
+        const usdPrice = Number(coin.priceUsd);
+        // converte em número o valor do preço de cada moeda
+
+        newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
+        // insere o texto para cada moeda, contendo o nome, símbolo e preço com 2 casas decimais
+
+        coinList.appendChild(newLi);
+        // adiciona um filho (li) na ul para cada moeda 
+    });
+
+}
+
+window.onload = () => insertDOM();
