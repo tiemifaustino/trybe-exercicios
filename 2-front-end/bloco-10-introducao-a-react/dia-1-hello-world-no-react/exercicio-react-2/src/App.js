e3e0bc4a277eb9285e7c52e3379abe856c43eb14
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-16 19:33:10 -0300

    Array "conteudos" renderizados com a função .map

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-react-2/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-react-2/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Content from './Content';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 }
---
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-react-2/src/Content.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const conteudos = [
+    {
+      conteudo: 'High Order Functions',
+      bloco: 8,
+      status: 'Aprendido'
+    },
+    {
+      conteudo: 'Composicao de Componentes',
+      bloco: 11,
+      status: 'Aprendendo',
+    },
+    {
+      conteudo: 'Composicao de Estados',
+      bloco: 12,
+      status: 'Aprenderei'
+    },
+    {
+      conteudo: 'Redux',
+      bloco: 16,
+      status: 'Aprenderei'
+    },
+  ];
+
+class Content extends React.Component {
+  render() {
+    return (
+        <div>
+          { 
+            conteudos.map((conteudo) => (
+              <div key={conteudo.conteudo}>
+                <h2>{`O conteúdo é: ${conteudo.conteudo}`}</h2>
+                <p>{`Status: ${conteudo.status}`}</p>
+                <p>{`Bloco: ${conteudo.bloco}`}</p> 
+              </div>
+            ))
+          }
+        </div>
+    );
+  }
+}
+
+export default Content;
