fe93d620b35d80ba0d2372b878a992f345adf793
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-16 18:59:06 -0300

    Componente Header renderizado no App.js

---
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-react-2/src/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Conteúdos de Front-End</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
