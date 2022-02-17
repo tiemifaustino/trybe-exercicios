5b13832f0f60d33f441cf904777031f130f1bf44
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-17 08:22:31 -0300

    Arquivo Footer renderizado

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-react-2/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-react-2/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
