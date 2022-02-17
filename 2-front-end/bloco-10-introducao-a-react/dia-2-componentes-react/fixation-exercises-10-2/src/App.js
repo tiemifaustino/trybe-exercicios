9e99689bd02d49d44d7b5268b27671e7bc0f0b14
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-17 18:44:09 -0300

    exercicio fixação 10.2 props feito

--- 2-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring"/>
     </div>
   );
 }
