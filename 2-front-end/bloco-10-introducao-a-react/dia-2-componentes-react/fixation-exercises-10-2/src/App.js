2ad930b6875e36892d4ec529884ba943c1380cd4
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-17 19:18:40 -0300

    Feito exercicio fixação composição de componentes

--- 2-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Image from './Image';
+// import Image from './Image';
+import Order from './Order';
 
-function App() {
-  return (
-    <div className="App">
-      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring"/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        {/* <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" /> */}
+        <h1> Orders recently created </h1>
+        <Order order={headphone} />
+        <Order order={energyDrink} />
+      </div>
+    );
+  }
 }
+
+// 1. O que o componente App é em relação a Order ?
+// Resposta: App é o componente pai e Order é o componente filho
 
 export default App;
