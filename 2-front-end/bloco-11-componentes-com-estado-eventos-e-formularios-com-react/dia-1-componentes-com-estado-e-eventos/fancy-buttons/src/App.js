2ee4c4dbe2894c3591b185a841509bd76a9403f1
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-22 09:05:32 -0300

    Exercício fixação - em andamento componentes com estados e eventos

--- 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -5,12 +5,21 @@ import React from 'react';
 class App extends React.Component {s
   constructor() {
     super();
+
+    this.state = {
+      numeroDeCliques: 0
+    }
+
     this.handleClick = this.handleClick.bind(this);
     this.handleClickButton2 = this.handleClickButton2.bind(this);
     this.handleClickButton3 = this.handleClickButton3.bind(this);
   }
   
   handleClick() {
+    this.setState({
+      numeroDeCliques: 1
+    })
+
     console.log('Clicou no botão', this);
   }
   
@@ -25,7 +34,7 @@ class App extends React.Component {s
   render() {
     return (
       <>
-        <button type="button" onClick={this.handleClick}>My Button</button>
+        <button type="button" onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
         <button type="button" onClick={this.handleClickButton2}>My Button 2</button>
         <button type="button" onClick={this.handleClickButton3}>My Button 3</button>
       </>
