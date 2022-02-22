d42338fcc603f537ea3c4374a35c48afbf7d7977
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-22 09:01:32 -0300

    Exercício fixação - vinculando funções à classe com this e bind no constructor

--- 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import React from 'react';
 
-function handleClick() {
-  console.log('Clicou no botão');
-}
 
-function handleClickButton2() {
-  console.log('Você clicou no botão 2');
-}
+class App extends React.Component {s
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickButton2 = this.handleClickButton2.bind(this);
+    this.handleClickButton3 = this.handleClickButton3.bind(this);
+  }
+  
+  handleClick() {
+    console.log('Clicou no botão', this);
+  }
+  
+  handleClickButton2() {
+    console.log('Você clicou no botão 2', this);
+  }
+  
+  handleClickButton3() {
+    console.log('Clicou no botão 3', this);
+  }
 
-function handleClickButton3() {
-  console.log('Clicou no botão 3');
-}
-
-class App extends React.Component {
   render() {
     return (
       <>
-        <button type="button" onClick={handleClick}>My Button</button>
-        <button type="button" onClick={handleClickButton2}>My Button 2</button>
-        <button type="button" onClick={handleClickButton3}>My Button 3</button>
+        <button type="button" onClick={this.handleClick}>My Button</button>
+        <button type="button" onClick={this.handleClickButton2}>My Button 2</button>
+        <button type="button" onClick={this.handleClickButton3}>My Button 3</button>
       </>
     );
   }
