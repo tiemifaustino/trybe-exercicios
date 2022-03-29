351b574b46d46388920fc9013dab45ce8c78e168
Tiemi Faustino <hayashipharma@gmail.com>
2022-03-29 09:09:06 -0300

    Link para login na pagina Home

--- 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/usando-redux-no-react-15.3-bonus/src/pages/Home.jsx
+++ 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/usando-redux-no-react-15.3-bonus/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
-
+        <h1>Bem-vindo!</h1>
+        <h2>Faça seu login</h2>
+        <Link to="/login">Login</Link>
       </div>
     );
   }
