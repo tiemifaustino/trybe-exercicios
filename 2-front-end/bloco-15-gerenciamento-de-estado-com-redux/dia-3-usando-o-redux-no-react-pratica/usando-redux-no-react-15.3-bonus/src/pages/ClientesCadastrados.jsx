a201032a9d2850c243511c9177bf3b54f65264b1
Tiemi Faustino <hayashipharma@gmail.com>
2022-03-29 11:08:00 -0300

    mapStateToProps para acessar o estado global no compon ClientesCadastrados - renderizaçao condicional

--- 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/usando-redux-no-react-15.3-bonus/src/pages/ClientesCadastrados.jsx
+++ 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/usando-redux-no-react-15.3-bonus/src/pages/ClientesCadastrados.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class ClientesCadastrados extends Component {
   render() {
+    const { email } = this.props;
+
     return (
       <div>
+
+        {
+          email === ''
+            ? <h2>Login não efetuado</h2>
+            : (
+              <h2>Clientes Cadastrados</h2>
+            )
+        }
         
       </div>
     );
   }
 }
 
-export default ClientesCadastrados;
+const mapStateToProps = (state) => ({
+  email: state.emailLoginReducer.email,
+})
+
+export default connect(mapStateToProps)(ClientesCadastrados);
