16ca129e77372e48e8c58edef179fda10c787845
Tiemi Faustino <hayashipharma@gmail.com>
2022-03-29 09:41:46 -0300

    Adicionado validaçao para os campos de email e senha

--- 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/usando-redux-no-react-15.3-bonus/src/pages/Login.jsx
+++ 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/usando-redux-no-react-15.3-bonus/src/pages/Login.jsx
@@ -16,12 +16,29 @@ class Login extends Component {
   // o value do input será o value da chave
   inputHandler = ({ target }) => {
     const { name, value } = target;
-    this.setState({
-      [name]: value,
-    })
+    this.setState({ [name]: value }, this.validateInputsForm)
+  }
+
+  validateInputsForm = () => {
+    const emailREGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g
+    const { email, password } = this.state;
+
+    // O método test() executa uma busca por uma correspondência entre uma expressão regular e uma string. Retorna true ou false.
+    // se o e-mail for válido e o campo de senha for maior ou igual a 6 caracteres, o button será ativado.
+    if (emailREGEX.test(email) && password.length >= 6) {
+      this.setState({
+        isButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isButtonDisabled: true,
+      });
+    }
   }
 
   render() {
+    const { isButtonDisabled } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
@@ -37,7 +54,7 @@ class Login extends Component {
             <input type="password" name="password" id="password" onChange={ this.inputHandler }/>
           </label>
 
-          <button type="submit">Entrar</button>
+          <button type="submit" disabled={ isButtonDisabled }>Entrar</button>
         </form>
       </div>
     );
@@ -45,3 +62,8 @@ class Login extends Component {
 }
 
 export default Login;
+
+/* 
+  Referência regex e-mail: https://regexr.com/3e48o
+  Método test(): https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+*/
