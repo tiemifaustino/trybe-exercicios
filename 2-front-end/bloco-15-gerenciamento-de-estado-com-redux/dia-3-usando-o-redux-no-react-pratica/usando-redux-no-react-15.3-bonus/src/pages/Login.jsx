97dd8c495c897d3ba53adb5ec84f27acb818631f
Tiemi Faustino <hayashipharma@gmail.com>
2022-03-29 09:24:20 -0300

    Adicionado handler para os inputs para salvar values no estado

--- 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/usando-redux-no-react-15.3-bonus/src/pages/Login.jsx
+++ 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/usando-redux-no-react-15.3-bonus/src/pages/Login.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      email: '',
+      password: '',
+      isButtonDisabled: true,
+    }
+  }
+
+  // ao digitar no input, o value será salvo no estado (onChange)
+  // o name do input será a chave
+  // o value do input será o value da chave
+  inputHandler = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    })
+  }
+
   render() {
     return (
       <div>
-        
+        <h1>Login</h1>
+
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input type="email" name="email" id="email" onChange={ this.inputHandler }/>
+          </label>
+
+          <label htmlFor="password">
+            Senha:
+            <input type="password" name="password" id="password" onChange={ this.inputHandler }/>
+          </label>
+
+          <button type="submit">Entrar</button>
+        </form>
       </div>
     );
   }
