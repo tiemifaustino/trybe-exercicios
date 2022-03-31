d59bcd4ec8dfc1fb3957fd46cca78157ea130c9f
Tiemi Faustino <hayashipharma@gmail.com>
2022-03-31 11:03:23 -0300

    Segundo teste course

--- 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/click-counter/src/App.test.js
+++ 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/click-counter/src/App.test.js
@@ -11,4 +11,19 @@ describe('testing clicks', () => {
     expect(buttonAdicionar).toBeInTheDocument();
     expect(screen.getByText('0')).toBeInTheDocument();
   });
+
+  // Caso seja interessante alterar o valor inicial do clickReducer , isso também é possível passando como segundo parâmetro para a função renderWithRedux um objeto com as propriedades que respeitem o formato original do state . Isto é:
+
+  // const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
+
+  // Observe: o objeto initialState representa o valor inicial do state.
+  // O clickReducer representa o reducer criado.
+  // O counter representa a propriedade que criamos dentro do nosso reducer .
+
+  test('a click in a button should increment the value of clicks', () => {
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
+
+    expect(screen.getByText('5')).toBeInTheDocument();
+  });
+
 });
