461ff53184f68b8ae12231d8eaa89afb9516e9c1
Tiemi Faustino <hayashipharma@gmail.com>
2022-06-08 09:11:37 -0300

    :heavy_check_mark: feat: Exercício 2

--- 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio1/exercicio1.js
+++ 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio1/exercicio1.js
@@ -14,14 +14,48 @@ function exercicioPromise(par1, par2, par3) {
   return promise;
 }
 
-exercicioPromise(1, 2, 3)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(`Erro: ${err.message}`));
+// exercicioPromise(1, 2, 3)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(`Erro: ${err.message}`));
 
-exercicioPromise(10, 20, 30)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(`Erro: ${err.message}`));
+// exercicioPromise(10, 20, 30)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(`Erro: ${err.message}`));
 
-exercicioPromise('a', 20, 30)
+// exercicioPromise('a', 20, 30)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(`Erro: ${err.message}`));
+
+
+function getNumbers() {
+  const par1 =  Math.floor(Math.random() * 100 + 1);
+  const par2 =  Math.floor(Math.random() * 100 + 1);
+  const par3 =  Math.floor(Math.random() * 100 + 1);
+
+  exercicioPromise(par1, par2, par3)
+    .then((result) => console.log('Resultado: %s', result))
+    .catch((err) => console.log('Erro: %s', err));
+}
+
+getNumbers();
+
+
+// Outra solução ************************************
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+function callDoMath() {
+  /* Criamos um novo array de 3 posições
+   * e utilizamos o `map` para gerar um número aleatório
+   * para cada posição do Array
+   */
+  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+  // console.log(randomNumbers);
+
+  return randomNumbers;
+}
+
+exercicioPromise(...callDoMath())
   .then((result) => console.log(result))
-  .catch((err) => console.log(`Erro: ${err.message}`));
+  .catch((err) => console.error(err.message))
