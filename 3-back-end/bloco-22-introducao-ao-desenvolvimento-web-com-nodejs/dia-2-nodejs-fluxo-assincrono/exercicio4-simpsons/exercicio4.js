3b7e54577db2e88b03117a230b20376e4f164a72
Tiemi Faustino <hayashipharma@gmail.com>
2022-06-08 10:11:39 -0300

    :heavy_check_mark: feat: Exercício 4.2

--- 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio4-simpsons/exercicio4.js
+++ 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio4-simpsons/exercicio4.js
@@ -21,3 +21,25 @@ async function read() {
 }
 
 read();
+
+// 4.2 Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
+
+async function getSimpsonById(id) {
+  const result = await fs.readFile(myFile, 'utf8');
+  const arraySimpsons = JSON.parse(result);
+
+  const simpson = arraySimpsons.find((simpson) => Number(simpson.id) === id);
+
+  if (!simpson) {
+    throw new Error('ID não encontrado');
+  }
+
+  return simpson;
+}
+
+async function main() {
+  const simpson = await getSimpsonById(2);
+  console.log(simpson);
+}
+
+main();
