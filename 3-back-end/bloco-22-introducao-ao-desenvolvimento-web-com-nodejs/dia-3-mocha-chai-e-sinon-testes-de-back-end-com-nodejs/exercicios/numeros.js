ae445045503f42113430e29513e0b7e11ec3b593
Tiemi Faustino <hayashipharma@gmail.com>
2022-06-09 10:15:19 -0300

    :heavy_check_mark: feat: Exercício 2

--- 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercicios/numeros.js
+++ 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercicios/numeros.js
@@ -2,18 +2,12 @@ function numerosNaturais(num) {
   switch (true) {
     case (num > 0):
       return 'positivo';
-      break;
     case (num < 0):
       return 'negativo';
-      break;
-    case (num === 0):
-      return 'neutro';
-      break;
-    default:
-      return num;
-      break;
-  }
 
+    default:
+      return 'neutro';
+  }
 }
 
 module.exports = numerosNaturais;
