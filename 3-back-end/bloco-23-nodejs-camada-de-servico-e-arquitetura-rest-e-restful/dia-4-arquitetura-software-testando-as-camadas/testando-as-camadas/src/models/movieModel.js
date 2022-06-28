204dfbe46c1faeba467c01547884ee10c90c39cb
Tiemi Faustino <hayashipharma@gmail.com>
2022-06-28 15:38:23 -0300

    feat: Testando camada de service, fazendo outro stub para isolar o Service, removendo a dependência ao Model.

--- 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-arquitetura-software-testando-as-camadas/testando-as-camadas/src/models/movieModel.js
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-arquitetura-software-testando-as-camadas/testando-as-camadas/src/models/movieModel.js
@@ -1,8 +1,7 @@
 const connection = require('./connection');
 
 const create = async ({ title, directedBy, releaseYear }) => {
-  const [result] = await connection
-    .execute(
+  const [result] = await connection.execute(
       "INSERT INTO model_example.movies (title, directed_by, release_year) VALUES (?, ?, ?)",
       [title, directedBy, releaseYear]
     );
