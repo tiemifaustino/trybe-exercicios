f9409ec3adc8d15835e62cee57a74b022b5feec3
Tiemi Faustino <hayashipharma@gmail.com>
2022-06-28 14:28:41 -0300

    feat: Dependencias instaladas Arquivo de teste models.test.js criado

---
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-arquitetura-software-testando-as-camadas/testando-as-camadas/tests/models/models.test.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+
+/*
+  Como ainda não temos a implementação, vamos fixar
+  um objeto simulando os métodos que iremos desenvolver,
+  porém, eles não terão nenhum comportamento
+*/
+const MoviesModel = {
+  create: () => {}
+};
+
+describe('Insere um novo filme no BD', () => {
+  const payloadMovie = {
+    title: 'Example Movie',
+    directedBy: 'Jane Dow',
+    releaseYear: 1999,
+  }
+
+  describe('quando é inserido com sucesso', () => {
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.be.a('object')
+    });
+
+    it('tal objeto possui o "id" do novo filme inserido', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.have.a.property('id')
+    });
+
+  });
+});
