41abaa971b60b2de14c3a779e3d46240b3de9e61
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-08 13:56:27 -0300

    :heavy_check_mark: feat: 🚀 Exercício 2: Crie o model Book utilizando a API do Sequelize

---
+++ 3-back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-orm-sequelize/src/models/book.js
@@ -0,0 +1,13 @@
+const Book = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    pageQuantity: DataTypes.INTEGER,
+  }, {
+    tablename: 'books',
+    underscored: true,
+  });
+  return Book;
+};
+
+module.exports = Book;
