717343511dcb21a617a065d313cee720099244ba
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-08 11:33:16 -0300

    :heavy_check_mark: feat: Exercício 1

---
+++ 3-back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-orm-sequelize/src/migrations/20220708140429-books.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('books', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        field: 'page_quantity'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'created_at'
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'updated_at'
+      },
+    });
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('books');
+  }
+};
