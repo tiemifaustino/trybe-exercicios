41c3f8df70849fcf8b1bef1b8c98420b4886fb41
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-11 10:12:39 -0300

    :heavy_check_mark: feat:
    Exercício 6: No service BooksService crie um método getById que recebe um id como parâmetro e use o model Book para buscar esse livro. Retorne o objeto encontrado pelo model.

--- 3-back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-orm-sequelize/src/services/booksService.js
+++ 3-back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-orm-sequelize/src/services/booksService.js
@@ -9,6 +9,11 @@ const booksService = {
 
   getById: async (id) => {
     const books = await db.Book.findByPk(id);
+    if (!books) {
+      const error = new Error('Book not found');
+      error.name = 'NotFoundError';
+      throw error;
+    }
     return books;
   },
 };
