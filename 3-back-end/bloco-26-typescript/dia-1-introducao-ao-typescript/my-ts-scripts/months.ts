cd2dd906a23dfebca4fece8d82a27fe3310c2397
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-26 19:15:57 -0300

    feat: :sparkles:
    Exercício 2: Crie uma enum que represente os meses do ano.

---
+++ 3-back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/months.ts
@@ -0,0 +1,16 @@
+enum Months {
+  January = "Janeiro",
+  February = "Fevereiro",
+  March = "Março",
+  April = "Abril",
+  May = "Maio",
+  June = "Junho", 
+  July = "Julho",
+  August = "Agosto",
+  September = "Setembro",
+  October = "Outubro",
+  November = "Novembro",
+  December = "Dezembro",
+}
+
+export default Months;
