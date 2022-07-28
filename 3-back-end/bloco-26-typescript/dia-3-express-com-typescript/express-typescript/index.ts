16002c9ea3d7d07e3e0aac3b16cf827e3bbaa0b4
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-28 15:28:45 -0300

    feat: :arrow_up:
    Criando conexão com o banco de dados para criar um CRUD de livros

--- 3-back-end/bloco-26-typescript/dia-3-express-com-typescript/express-typescript/index.ts
+++ 3-back-end/bloco-26-typescript/dia-3-express-com-typescript/express-typescript/index.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import 'express-async-errors';
+import 'express-async-errors';  
+import BooksRoutes from './routes/books.routes';
 
 const app = express();
 
@@ -8,9 +9,11 @@ app.use(express.json());
 
 const PORT = 8000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('Express + TypeScript')
 });
+
+app.use(BooksRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   const { name, message, details } = err as any;
