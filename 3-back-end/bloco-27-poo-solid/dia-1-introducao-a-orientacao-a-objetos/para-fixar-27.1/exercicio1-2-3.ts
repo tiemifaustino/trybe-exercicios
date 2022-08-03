a290d40250131d7be96ac527a0d0e46fc27aedd6
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-03 11:52:34 -0300

    feat: :sparkles:
    7 - Defina um valor para o atributo _connectedTo por meio do método setter criado e imprima seu valor.

--- 3-back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/para-fixar-27.1/exercicio1-2-3.ts
+++ 3-back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/para-fixar-27.1/exercicio1-2-3.ts
@@ -21,27 +21,34 @@ class Tv {
     Em caso positivo, definir este valor para o atributo _connectedTo;
     Em caso negativo, imprimir no console a mensagem "Sorry, connection unavailable!"
   */
+  // getter: ACESSA OS ATRIBUTOS DA CLASSE
   get connectedTo(): string | undefined {
     return this._connectedTo;
   }
 
+  // setter: MODIFICA OS ATRIBUTOS DA CLASSE
   set connectedTo(newConnection: string | undefined) {
     // O condicional permite setar undefined ou uma conexão que esteja no `connections`
     if (!newConnection || this._connections.includes(newConnection)) {
       this._connectedTo = newConnection
       console.log(this._connectedTo);
-      
+    } else {
+      console.log("Sorry, connection unavailable!");
     }
-    console.log("Sorry, connection unavailable!");
   }
 }
-
-
 
 // cria um objeto com a palavra reservada "new"
 const television = new Tv('Samsung', 50, '4k', ['wifi', 'bluetooth']);
 
 // chama o método "turnOn" da classe Tv
 television.turnOn();
+
+// define valor para o atributo _connectedTo por meio do método setter
+television.connectedTo = 'wifi';
+
+// imprime valor declarado na linha 47 pelo método getter
+console.log('Connected to', television.connectedTo);
+
 
 // executar com "npm start"
