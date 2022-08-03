0e6dd0fbc6866d8fce90d8cce7631e5e2b930389
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-03 18:55:35 -0300

    feat: :sparkles:
    Criando a classe School

--- 3-back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/para-fixar-27.1/exercicio.pratica.ts
+++ 3-back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/para-fixar-27.1/exercicio.pratica.ts
@@ -12,4 +12,46 @@ class School {
     this._testGrades = t;
     this._workGrades = w;
   }
+
+  get name(): string {
+    return this._name;
+  }
+  
+  set name(value: string) {
+    if (value.length < 3) {
+      console.log('The name must have 3 characters at least');
+    }
+    this._name = value;
+  }
+
+  get enrollment(): string {
+    return this._enrollment;
+  }
+
+  set enrollment(value: string) {
+    this._enrollment = value;
+  }
+
+  get testGrades(): number[] {
+    return this._testGrades;
+  }
+
+  set testGrades(value: number[]) {
+    if (value.length > 4) {
+      console.log('The student must have 4 test grades only');
+    }
+    this._testGrades = value;
+  }
+
+  get workGrades(): number [] {
+    return this._workGrades;
+  }
+
+  set workGrades(value: number[]) {
+    if (value.length > 2) {
+      console.log('The student must have 2 work grades only');
+    }
+    this._workGrades = value;
+  }
 }
+
