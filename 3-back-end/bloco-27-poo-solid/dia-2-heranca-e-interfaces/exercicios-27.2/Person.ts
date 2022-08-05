afb684288b8164c47c66c094a87f37ac7521f3db
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-05 10:10:57 -0300

    feat: :sparkles:
    Criando um metodo private para validar o nome

--- 3-back-end/bloco-27-poo-solid/dia-2-heranca-e-interfaces/exercicios-27.2/Person.ts
+++ 3-back-end/bloco-27-poo-solid/dia-2-heranca-e-interfaces/exercicios-27.2/Person.ts
@@ -1,14 +1,12 @@
 class Person {
-  constructor(private _name: string, private _birthDate: Date) {}
+  constructor(public _name: string, private _birthDate: Date) {}
 
   get name() {
     return this._name;
   }
 
   set name(value: string) {
-    if (value.length < 3) {
-      throw new Error('The name must have 3 characters at least');
-    }
+    this.validateName(value);
     this._name = value;
   }
 
@@ -19,4 +17,19 @@ class Person {
   set birthDate(value: Date) {
     this._birthDate = value;
   }
+
+  private validateName(value: string) {
+    if (value.length < 3) {
+      throw new Error('The name must have 3 characters at least');
+    }
+  }
 }
+
+const person1 = new Person('Tiemi', new Date(1990, 3, 26));
+console.log(person1);
+console.log(person1.birthDate);
+// new Date() -> retorna data atual (hoje)
+// getTime() -> retorna Um número representando os milissegundos passados entre 1 de Janeiro de 1970 00:00:00 UTC e a data atual
+// console.log(new Date().getTime());
+console.log(Math.abs(Date.now() - new Date(1990, 3, 26).getTime()));
+
