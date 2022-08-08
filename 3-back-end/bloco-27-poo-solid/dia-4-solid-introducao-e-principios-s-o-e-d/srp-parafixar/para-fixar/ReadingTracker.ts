72836e0e333cc87dcd2b9fecb2993b920f2f15d0
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-08 11:25:26 -0300

    feat: :sparkles:
    Classes separadas por finalidade seguindo o "S" - Single Responsibility Principle

--- 3-back-end/bloco-27-poo-solid/dia-4-solid-introducao-e-principios-s-o-e-d/srp-parafixar/para-fixar/ReadingTracker.ts
+++ 3-back-end/bloco-27-poo-solid/dia-4-solid-introducao-e-principios-s-o-e-d/srp-parafixar/para-fixar/ReadingTracker.ts
@@ -1,30 +1,42 @@
+// 1 - Observe o código abaixo e faça as alterações necessárias para que ele passe a respeitar o Princípio da Responsabilidade Única (SRP).
+// eslint-disable-next-line max-classes-per-file
 type Book = {
   book: string;
   author: string;
   genre: string;
 }
 
+// Imprime a mensagem resultante da classe ReadingTracker
+function progressNotification(message: string): void {
+  console.log(message);
+}
+
+// Mostra se já leu a quantidade de livros que foram colocados como objetivo
 class ReadingTracker {
   private readingGoal: number;
   private booksRead: number;
-  private wishlist: Book[];
+
   constructor(readingGoal: number) {
     this.readingGoal = readingGoal;
     this.booksRead = 0;
-    this.wishlist = [];
   }
-
+  
   trackReadings(readsCount: number): void {
     this.booksRead += readsCount;
     if (this.booksRead >= this.readingGoal) {
-      this.progressNotification(
-        'Congratulations! You\'ve reached your reading goal!',
-      );
+      progressNotification('Congratulations! You\'ve reached your reading goal!');
       return;
     }
-    this.progressNotification(
-      'There are still some books to go!',
-    );
+    progressNotification('There are still some books to go!');
+  }
+}
+
+// Adiciona e mostra os livros da lista de desejos
+class BooksWishList {
+  private wishlist: Book[];
+  constructor(book: Book) {
+    this.wishlist = [];
+    this.wishlist.push(book);
   }
 
   addToWishlist(book: Book): void {
@@ -34,14 +46,24 @@ class ReadingTracker {
   showWishlist(): void {
     console.log(this.wishlist);
   }
-
-  progressNotification(message: string): void {
-    console.log(message);
-  }
 }
 
-const readTracker = new ReadingTracker(20);
-readTracker.addToWishlist({ book: 'The Road', author: 'Cormac McCarthy', genre: 'Dystopia' });
-readTracker.showWishlist();
-readTracker.trackReadings(12);
-readTracker.trackReadings(9);
+const readTracker = new ReadingTracker(20); // instancia a classe de leitura de livros com a quantidade de leitura a ser atinginda (20 livros)
+readTracker.trackReadings(12); // There are still some books to go! // Adiciona 12 livros lidos na instância criada da classe ReadingTracker (readTracker)
+readTracker.trackReadings(9); // Congratulations! You've reached your reading goal!
+
+const wishlist = new BooksWishList({ // instancia uma nova classe de BooksWishList
+  book: 'The Road',
+  author: 'Cormac McCarthy',
+  genre: 'Dystopia' });
+
+wishlist.addToWishlist({ // adiciona livro a lista de desejos
+  book: 'Misery',
+  author: 'Stephen King',
+  genre: 'Thriller' });
+
+wishlist.showWishlist(); // mostra a lista de desejos
+// [
+//   { book: 'The Road', author: 'Cormac McCarthy', genre: 'Dystopia' },
+//   { book: 'Misery', author: 'Stephen King', genre: 'Thriller' }
+// ]
