2ce1f2ed7a743da13d98477e641e8f5a5790d692
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-08 09:11:03 -0300

    feat: :sparkles:
    Cria um novo tipo para uma escola e remodela a função approvedStudents para ficar totalmente genérica. Agora podemos acrescentar mais uma escola que tenha a nota de corte de aprovação diferente

--- 3-back-end/bloco-27-poo-solid/dia-4-solid-introducao-e-principios-s-o-e-d/srp-parafixar/src/index.ts
+++ 3-back-end/bloco-27-poo-solid/dia-4-solid-introducao-e-principios-s-o-e-d/srp-parafixar/src/index.ts
@@ -4,9 +4,16 @@ type Discipline = {
   letterGrade?: string;
 };
 
+type School = {
+  name: string;
+  approvalGrade: number;
+};
+
 type Student = {
   name: string;
   disciplines: Discipline[];
+  // school?: string;
+  school: School; // Agora não é mais uma string
 };
 
 /* Apoio para a função `getGradeLetter` */
@@ -36,8 +43,11 @@ const percentageGradesIntoLetters = (student: Student): Student => ({
   disciplines: student.disciplines.map(getLetterGrades) });
 
 /* "Determinar" */
-const approvedStudents = ({ disciplines }: Student): boolean =>
-  disciplines.every(({ grade }) => grade > 0.7);
+// const approvedStudents = ({ disciplines }: Student): boolean =>
+//   disciplines.every(({ grade }) => grade > 0.7);
+
+const approvedStudents = ({ disciplines, school }: Student): boolean =>
+  disciplines.every(({ grade }) => grade >= school.approvalGrade);
 
 /* "Atualizar" */
 const updateApprovalData = (student: Student): void => {
@@ -62,29 +72,34 @@ export {
   getLetterGrades,
 };
 
-/* Abaixo temos um exemplo de execução */
-const students = [
+// Para testar:
+const studentsExample = [
   {
     name: 'Lee',
-    disciplines: [
-      { name: 'matemática', grade: 0.8 },
-      { name: 'história', grade: 0.6 },
-    ],
-  },
-  {
-    name: 'Clementine',
+    school: { name: 'Standard', approvalGrade: 0.7 },
     disciplines: [
       { name: 'matemática', grade: 0.8 },
       { name: 'história', grade: 0.9 },
     ],
   },
+  {
+    name: 'Albus',
+    school: { name: 'Hogwarts', approvalGrade: 0.8 },
+    disciplines: [
+      { name: 'divination', grade: 0.8 },
+      { name: 'potions', grade: 0.9 },
+    ],
+  },
 ];
 
-setApproved(students);
+setApproved(studentsExample);
 
-/*
-Saída:
-A pessoa com nome Clementine foi aprovada!
-matemática: B
-história: A
+/* 
+  npx ts-node src/index.ts 
+  A pessoa com nome Lee foi aprovada!
+  matemática: B
+  história: A
+  A pessoa com nome Albus foi aprovada!
+  divination: B
+  potions: A
 */
