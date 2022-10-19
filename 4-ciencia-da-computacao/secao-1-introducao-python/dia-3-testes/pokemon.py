c70628df6053a03219f87fa93950094d3ac6eed0
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-19 15:14:35 -0300

    feat: Adiciona módulo a ser testado pokemon.py

---
+++ 4-ciencia-da-computacao/secao-1-introducao-python/dia-3-testes/pokemon.py
@@ -0,0 +1,23 @@
+import json
+
+
+def retrieve_pokemons_by_type(type, reader):
+    # lê o conteudo de reader e o transforma de json
+    # para dicionário
+    pokemons = json.load(reader)["results"]
+    # filtra somente os pokemons do tipo escolhido
+    pokemons_by_type = [
+        pokemon for pokemon in pokemons if type in pokemon["type"]
+    ]
+    return pokemons_by_type
+
+
+# Um segundo cenário é onde a função espera o nome de um arquivo e a abertura
+# do mesmo acontece dentro da função.
+def retrieve_pokemons_by_type2(type, filepath):
+    with open(filepath) as file:
+        pokemons = json.load(file)["results"]
+        pokemons_by_type = [
+            pokemon for pokemon in pokemons if type in pokemon["type"]
+        ]
+        return pokemons_by_type
